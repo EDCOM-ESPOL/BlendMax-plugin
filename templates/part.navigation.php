82671bf4ea31219616ff9795e4db773102b174f5
PaulValle <paul.valle96@gmail.com>
2018-04-16 11:15:35 -0500

    Templates

---
+++ templates/part.navigation.php
@@ -0,0 +1,11 @@
+<div class="jobnav-top">
+	<a id="closeNav">Opciones<span class="fa fa-caret-up" aria-hidden="true"></span></a>
+	<a id="openNav">Opciones<span class="fa fa-caret-down" aria-hidden="true"></span></a>
+</div>
+<ul>
+	<li><a id="endJob" href="#">Trabajos Finalizados</a></li>
+	<li><a id="processJob" href="#">Trabajos en Proceso</a></li>
+    <li><a id="render-edcom" class="selectedOption" href="/owncloud/index.php/apps/renderedcom/">Render Blender</a></li>
+	<li><a id="renderMax" class="selectedOption" href="/owncloud/index.php/apps/rendermax/">Render 3dMax</a></li>
+    <li><a id="renderMax" class="selectedOption" href="/owncloud/index.php/apps/renderblendmax/">Render BlendMax</a></li>
+</ul>
